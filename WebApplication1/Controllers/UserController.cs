c1619bc9ab3fcb770c98ad87d97ad63b8cb6d6a0
qwe821526_411018141 <qwe821526@gmail.com>
2023-06-07 19:57:02 +0800

    exam

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Music()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Music(string name,float num)
+        {
+            var r = "";
+            if (num.HasValue)
+            {
+                r = "cc";
+            }
+            ViewBag.Name = name;
+            ViewBag.Num = r;
+            return View();
+        }
+    }
+}
